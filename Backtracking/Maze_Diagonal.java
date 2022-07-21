1137b0b82418db7fc611b5a5ea906cff7635a4d3
Sachin Kumar Pal <56400187+sachinpal293@users.noreply.github.com>
2022-07-21 12:09:30 +0530

    Backtraking Question

--- Backtracking/Maze_Diagonal.java
+++ Backtracking/Maze_Diagonal.java
@@ -2,12 +2,13 @@ package com.Backtracking;
 
 import java.util.ArrayList;
 
-public class Maze_path_list {
+public class Maze_Diagonal {
     public static void main(String[] args) {
 
-        System.out.println(pathlist("",3,3));
+        System.out.println(pathlistdiagnol("",3,3));
     }
-    static ArrayList<String> pathlist(String p, int r, int c)
+
+    static ArrayList<String> pathlistdiagnol(String p, int r, int c)
     {
         if(r==1 && c==1)
         {
@@ -18,14 +19,17 @@ public class Maze_path_list {
         ArrayList<String> list = new ArrayList<>();
         if(r>1)
         {
-            list.addAll(pathlist(p+'V',r-1,c));
+            list.addAll(pathlistdiagnol(p+'V',r-1,c));
         }
         if(c>1)
         {
-            list.addAll(pathlist(p+'H',r,c-1));
+            list.addAll(pathlistdiagnol(p+'H',r,c-1));
+        }
+        if(r>1 & c>1)
+        {
+            list.addAll(pathlistdiagnol(p+'D',r-1,c-1));
         }
 
         return list;
     }
-
 }
