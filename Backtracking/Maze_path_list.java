85dc0d2e4d9b6836c47e6b504690c6220bbebb3a
Sachin Kumar Pal <56400187+sachinpal293@users.noreply.github.com>
2022-07-21 11:56:41 +0530

    Q1 type 3

---
+++ Backtracking/Maze_path_list.java
@@ -0,0 +1,30 @@
+package com.Backtracking;
+
+import java.util.ArrayList;
+
+public class Maze_path_list {
+    public static void main(String[] args) {
+
+        System.out.println(pathlist("",3,3));
+    }
+    static ArrayList<String> pathlist(String p, int r, int c)
+    {
+        if(r==1 && c==1)
+        {
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        ArrayList<String> list = new ArrayList<>();
+        if(r>1)
+        {
+            list.addAll(pathlist(p+'D',r-1,c));
+        }
+        if(c>1)
+        {
+            list.addAll(pathlist(p+'R',r,c-1));
+        }
+
+        return list;
+    }
+}
