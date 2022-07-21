7c2b4c378f44e1f797f1c6643db9af782bb91e2b
Sachin Kumar Pal <56400187+sachinpal293@users.noreply.github.com>
2022-07-21 13:22:13 +0530

    Must Practice it

--- Backtracking/allpath_print_martix.java
+++ Backtracking/allpath_print_martix.java
@@ -2,22 +2,27 @@ package com.Backtracking;
 
 import java.util.Arrays;
 
-public class allpath {
+public class allpath_print_martix {
     public static void main(String[] args) {
         boolean[][] board = {
                 {true,true,true},
                 {true,true,true},
                 {true,true,true}
         };
-
-        allpath("",board,0,0);
-
+        int[][] path = new int[board.length][board[0].length];
+        allpathmatrix("",board,0,0,path, 1);
     }
-    static void allpath(String p,boolean[][] maze,int r, int c)
+    static void allpathmatrix(String p,boolean[][] maze,int r, int c, int[][] path , int step)
     {
         if(r== maze.length-1 && c == maze[0].length-1)
         {
+            path[r][r] = step;
+            for(int[] arr:path)
+            {
+                System.out.println(Arrays.toString(arr));
+            }
             System.out.println(p);
+            System.out.println();
             return;
         }
 
@@ -27,27 +32,27 @@ public class allpath {
         }
         // i am considering this block in my path
         maze[r][c]  = false;
+        path[r][c] = step;
         if(r<maze.length-1)
         {
-            allpath(p+'D',maze,r+1,c);
+            allpathmatrix(p+'D',maze,r+1,c,path,step+1);
         }
         if(c<maze.length-1)
         {
-            allpath(p+'R',maze,r,c+1);
+            allpathmatrix(p+'R',maze,r,c+1,path,step+1);
         }
         if(r>0)
         {
-            allpath(p+'U',maze,r-1,c);
+            allpathmatrix(p+'U',maze,r-1,c,path,step+1);
         }
         if(c>0)
         {
-            allpath(p+'L',maze,r,c-1);
+            allpathmatrix(p+'L',maze,r,c-1,path,step+1);
         }
 
         // this line is where the function will be over before the function
         // gets removed, also remove the changes that were made by function
         maze[r][c] = true;
+        path[r][c] = 0;
     }
-
-
 }
