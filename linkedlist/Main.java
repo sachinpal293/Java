99a1402ba5758e536811ab5605e80ba5938c1831
Sachin Kumar Pal <56400187+sachinpal293@users.noreply.github.com>
2022-09-24 10:05:28 +0530

    Add files via upload

---
+++ linkedlist/Main.java
@@ -0,0 +1,12 @@
+package com.linkedlist;
+
+public class Main {
+    public static void main(String[] args) {
+     LL list = new LL();
+     list.insertFirst(3);
+     list.insertFirst(2);
+     list.insertFirst(85);
+
+     list.display();
+    }
+}
